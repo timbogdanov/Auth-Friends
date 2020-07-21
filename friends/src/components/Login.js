774d9178d12d2ab8044dd3cf6d13ed7a8fe967ac
Timofey Bogdanov <timofeybogdanov@Timofeys-iMac.local>
2020-07-21 13:47:24 -0700

    login saves token to localStorage, added loading gif to button

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,20 +1,34 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
-const Login = () => {
+const Login = (props) => {
   const initialState = {
     username: 'tim',
     password: 'tim',
   };
 
+  const [isLoading, setIsLoading] = useState(false);
   const [credentials, setCredentials] = useState(initialState);
 
   const onChange = (e) => {
     setCredentials({ [e.target.name]: e.target.value });
   };
 
+  const login = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    setTimeout(() => {
+      axios.post('http://localhost:5000/api/login', credentials).then((res) => {
+        localStorage.setItem('token', res.data.payload);
+        props.history.push('/protected');
+      });
+    }, [1000]);
+  };
+
   return (
     <div>
-      <form>
+      <form onSubmit={login}>
         <input
           type='text'
           name='username'
@@ -31,7 +45,15 @@ const Login = () => {
           value={credentials.password}
           className='input'
         />
-        <button className='button'>Button</button>
+        <button className='button'>
+          {isLoading ? (
+            <span>
+              <img src='https://thumbs.gfycat.com/AgileChubbyGelada-size_restricted.gif'></img>
+            </span>
+          ) : (
+            <span>Button</span>
+          )}
+        </button>
       </form>
     </div>
   );
